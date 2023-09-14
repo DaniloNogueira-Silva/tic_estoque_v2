40fec5ef28d9860e9c2dbeea12a8ed1fc0a2edba
DaniloNogueira-Silva <dansilva730x@gmail.com>
2023-09-14 20:51:02 -0300

    fix: Ajustando date orderItem

--- src/interfaces/controllers/order.controller.ts
+++ src/interfaces/controllers/order.controller.ts
@@ -173,10 +173,12 @@ export class OrderController {
 
   createOrderItem: RequestHandler = async (req, res) => {
     try {
+      
       const { order_items } = req.body as { order_items: Order_item[] };
 
       const updatedOrderItems = await Promise.all(
         order_items.map(async (orderItemData) => {
+          
           const product = await this.repositoryProduct.getById(
             orderItemData.productId
           );
@@ -185,10 +187,20 @@ export class OrderController {
               `Produto com ID ${orderItemData.productId} não encontrado`
             );
           }
+          
+          const expectedDate = new Date(orderItemData.expected_date);
 
-          const updatedOrderItem = await this.repository.create_order_item(
-            orderItemData
-          );
+          const updatedOrderItem = await this.repository.create_order_item({
+            id: orderItemData.id,
+            status: orderItemData.status,
+            expected_date: expectedDate,
+            quantityInStock: orderItemData.quantityInStock,
+            newQuantity: orderItemData.newQuantity,
+            orderId: orderItemData.orderId,
+            productId: orderItemData.productId
+          }
+            
+          )
           if (orderItemData.status == "chegou") {
             await this.repositoryProduct.update(orderItemData.productId, {
               id: orderItemData.productId,
